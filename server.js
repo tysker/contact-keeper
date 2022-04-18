cd0e3923e2e6494e5037edd07ef57071cb70d77a
Jörg Oertel <tyskerjoerg@gmail.com>
2022-04-18 10:15:39 +0200

    finish

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/database');
+const path = require('path');
 
 const app = express();
 
@@ -19,6 +20,16 @@ app.get('/', (req, res) =>
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Server static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/dist'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+  );
+}
 
 // search first for env variable called PORT
 const PORT = process.env.PORT || 5000;
